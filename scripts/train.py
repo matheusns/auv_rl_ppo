6f19b09752ae45eb37f322bef84fa7d81e5df91a
Matheus Santos <contamatheusnascimento@gmail.com>
2023-09-14 15:30:25 +0400

    Enable agent training

--- scripts/train.py
+++ scripts/train.py
@@ -34,6 +34,6 @@ if __name__ == "__main__":
     rospy.logdebug (f"model_name = {model_name}")
     
     ppo = PPOTrainer(timesteps, log_dir)
-    # ppo.learn()
-    # ppo.save(os.path.join(save_dir, model_name))
+    ppo.learn()
+    ppo.save(os.path.join(save_dir, model_name))
     rospy.spin()
